c7c3e68530e124b6f9d8f9d7bc70df7130a3b5ef
Manan Gupta <manangupta27.mg@gmail.com>
2025-05-08 09:39:52 +0530

    [Manan] ADD:Balanced brackets

---
+++ Problem3.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Problem3 {
+    public static boolean isParenthesesBalanced(String expression) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : expression.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else if (ch == ')' || ch == '}' || ch == ']') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if ((ch == ')' && top != '(') ||
+                        (ch == '}' && top != '{') ||
+                        (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            String s1 = sc.next();
+            if (isParenthesesBalanced(s1)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+
+        }
+    }
+}
