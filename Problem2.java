6f0c0d0cce207918e632fe70c5ffaa7c7c6bc9ac
Manan Gupta <manangupta27.mg@gmail.com>
2025-05-14 09:12:58 +0530

    [Manan] ADD:Set matrix to zero

---
+++ Problem2.java
@@ -0,0 +1,23 @@
+class Problem2 {
+    public void setZeroes(int[][] matrix) {
+        int[] rows = new int[matrix.length];
+        int[] columns = new int[matrix[0].length];
+
+        for (int i=0; i<matrix.length; i++) {
+            for (int j=0; j<matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    rows[i] = 1;
+                    columns[j] = 1;
+                }
+            }
+        }
+
+        for (int i=0; i<matrix.length; i++) {
+            for (int j=0; j<matrix[0].length; j++) {
+                if (rows[i]==1 || columns[j]==1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
